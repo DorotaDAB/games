8a15954842a9b37a933de3aafee31f6b5f92703c
DorotaDAB <dorota.dabrowska.kowalska@gmail.com>
2019-09-23 19:42:33 +0200

    Fix const

--- src/config/constans.js
+++ src/config/constans.js
@@ -1,6 +1,6 @@
 export default {
-  USER_LEVEL_BEGINNER = "Beginner",
-  USER_LEVEL_REGULAR = "Regular",
-  USER_LEVEL_ADVANCED = "Advanced",
-  USER_LEVEL_CRAZY = "Crazy",
+  USER_LEVEL_BEGINNER: "Beginner",
+  USER_LEVEL_REGULAR: "Regular",
+  USER_LEVEL_ADVANCED: "Advanced",
+  USER_LEVEL_CRAZY: "Crazy",
 };
