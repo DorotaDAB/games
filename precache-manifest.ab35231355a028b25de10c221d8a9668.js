5587399e58874dec765b8d1f74a2766c7b14645e
DorotaDAB <dorota.dabrowska.kowalska@gmail.com>
2019-09-24 09:46:10 +0200

    Updates

---
+++ precache-manifest.ab35231355a028b25de10c221d8a9668.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce04146b4e7c207ca8ecb09f154de608",
+    "url": "/games/index.html"
+  },
+  {
+    "revision": "c1cb143b37269ef98bec",
+    "url": "/games/static/css/main.6d038c70.chunk.css"
+  },
+  {
+    "revision": "95e874717f3b026da844",
+    "url": "/games/static/js/2.fb446755.chunk.js"
+  },
+  {
+    "revision": "c1cb143b37269ef98bec",
+    "url": "/games/static/js/main.340a0645.chunk.js"
+  },
+  {
+    "revision": "abab096e72f69d49d270",
+    "url": "/games/static/js/runtime~main.ac9a7541.js"
+  },
+  {
+    "revision": "c029276c6cb29db2586937c311d7d186",
+    "url": "/games/static/media/tic-tac-toe.c029276c.png"
+  }
+]);
